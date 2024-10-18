a9ba35b202563bb981cf9b8c34e28c58763b732d
Jiten Rai <raijiten96@gmail.com>
2024-10-18 07:57:16 +0545

    for vercel

--- django_revised/django_revised/settings.py
+++ django_revised/django_revised/settings.py
@@ -140,6 +140,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates', 'static')
