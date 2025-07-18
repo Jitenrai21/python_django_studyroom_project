29b3f4d5a9efb2615070f14badd3a928e4c5f9e4
Jiten Rai <152976119+Jitenrai21@users.noreply.github.com>
2025-07-18 09:55:14 +0545

    Update settings.py

--- StudyRoomProject/StudyRoom/settings.py
+++ StudyRoomProject/StudyRoom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lnv55z6=+5jpgrc!o_306i#z@^-cvmokg_v0!(d)3l)ldpxq0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
